bc92b65747664634bd1122f2644f3d21820bec93
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-29 14:56:24 -0400

    first commit

---
+++ src/commands/commands/voiceline.js
@@ -0,0 +1,25 @@
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "voiceline",
+        description: "Sends a random Quag voiceline",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+        const voicelinesJsonData = await fs.readFile("data/voicelines.json", {encoding: "utf8"});
+        const voicelinesMap = JSON.parse(voicelinesJsonData);
+        const voicelines = voicelinesMap["voicelines"];
+        const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
+            
+        await interaction.reply({
+            content: `${voiceline.text}`,
+            files:[{
+                attachment: `sounds/${voiceline.path}`,
+                name: `${voiceline.path}`
+            }],
+            ephemeral: false
+        });
+      },
+}
